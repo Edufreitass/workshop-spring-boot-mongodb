9b4933a24979997931ba790c083997afa4e15eb5
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-08-28 09:41:59 -0300

    Consulta simples com Query Methods

--- src/main/java/com/nelioalves/workshop/mongo/repository/PostRepository.java
+++ src/main/java/com/nelioalves/workshop/mongo/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.nelioalves.workshop.mongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.nelioalves.workshop.mongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
